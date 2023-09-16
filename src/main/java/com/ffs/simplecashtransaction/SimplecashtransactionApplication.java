f52fa467da5c8589d14b290b85a3800a0f0fe866
Fábio Faleiros de Siqueira <fabiofaleiros@gmail.com>
2023-09-16 18:52:59 +0100

    first commit

---
+++ src/main/java/com/ffs/simplecashtransaction/SimplecashtransactionApplication.java
@@ -0,0 +1,13 @@
+package com.ffs.simplecashtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecashtransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecashtransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ffs/simplecashtransaction/SimplecashtransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffs.simplecashtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecashtransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
